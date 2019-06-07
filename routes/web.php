792137253f1fcd34c35af27f9bcffe50e64f3e72
Matt J <matt.jellings@gmail.com>
2019-06-07 14:44:53 +0100

    enabled registrations

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Auth::routes(['register' => false]);
 Auth::routes(['register' => false]);
 
 Route::group(['middleware' => 'auth'], function () {
